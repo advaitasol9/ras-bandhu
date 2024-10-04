1bfbefeed3ba4bd6546557d4d8890fe066f86ced
advaitasol9 <advaitasol9@gmail.com>
2024-10-04 20:08:26 +0530

    stage env setup

--- components/context/firebase-providers.tsx
+++ components/context/firebase-providers.tsx
@@ -19,13 +19,13 @@ import { getAnalytics } from "firebase/analytics";
 import { FirebaseOptions } from "firebase/app";
 
 const config: FirebaseOptions = {
-  apiKey: "AIzaSyAD9kVGBT-lGrJfTdsdIOEAIRMI7S_ZuTM",
-  authDomain: "ras-bandhu.firebaseapp.com",
-  projectId: "ras-bandhu",
-  storageBucket: "ras-bandhu.appspot.com",
-  messagingSenderId: "645936493889",
-  appId: "1:645936493889:web:62f0cd447409d263bfddc0",
-  measurementId: "G-GH51H7LPDX",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const FirebaseProviderSDKs: FC<{ children: ReactNode }> = ({ children }) => {
